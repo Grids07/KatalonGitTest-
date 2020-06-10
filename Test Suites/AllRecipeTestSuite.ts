4d94e420234befab85ead7c4284acb971b648a71
iclark <iclark@CVG-000211M-117.ga.afginc.com>
2020-06-10 08:40:44 -0400

    added 2 test cases for All Recipe

---
+++ Test Suites/AllRecipeTestSuite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AllRecipeTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>48e5ef95-e28a-4f36-9c49-ded1e66b1cb1</testSuiteGuid>
+   <testCaseLink>
+      <guid>be296031-170a-4157-b44e-04c001449131</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestingGit</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>62584c74-41b1-42f0-b1c4-88bb14c8f3e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestingGit - Copy</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
